6cd0a95eb8ec5055894bc9a50fc8f9592c9b1e47
jaygaur7 <gjb41098@gmail.com>
2020-07-17 23:29:33 +0530

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+  $(window).scroll(function() {
+         if ( $(this).scrollTop() > 50 ) {
+             $('.navbar').addClass('solid bg-dark');
+         } else {
+             $('.navbar').removeClass('solid bg-dark');
+         }
+     });
+ 
+ });
